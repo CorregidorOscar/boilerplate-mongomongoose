4bbd0d621716e8350ffd1ec3943d92f933ea673f
CorregidorOscar <corregidoroscar18@gmail.com>
2022-01-03 12:51:36 -0300

    Instalación y configuración Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
